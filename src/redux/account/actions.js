c125501906f0dc2a4f61637be67e9c01ab5cac2a
tom1700 <tomekm1700@gmail.com>
2017-11-19 00:08:15 +0100

    Using flow in action creators.

--- src/redux/account/actions.js
+++ src/redux/account/actions.js
@@ -1,17 +1,19 @@
+// @flow
+import type { Account, NewAccount } from './schema';
 import { actions } from './constants';
 
 
-export const addAccount = ({ name, balance, currency }) => ({
+export const addAccount = ({ name, balance, currency } : NewAccount) => ({
     type: actions.ADD_ACCOUNT,
     payload: { name, balance, currency }
 });
 
-export const removeAccount = ({ id }) => ({
+export const removeAccount = ({ id } : { id : number }) => ({
     type: actions.REMOVE_ACCOUNT,
     payload: { id }
 });
 
-export const updateAccount = ({ id, name, balance, currency }) => ({
+export const updateAccount = ({ id, name, balance, currency } : Account) => ({
     type: actions.UPDATE_ACCOUNT,
     payload: { id, name, balance, currency }
 });
--- src/redux/account/schema.js
+++ src/redux/account/schema.js
@@ -1,6 +1,12 @@
 // @flow
 import type { Currency } from './constants';
 
+export type NewAccount = {
+    name: string,
+    balance: number,
+    currency: Currency
+};
+
 export type Account = {
     id: number,
     name: string,
