8439001229c7e3f89f3bca04e39469c36da0a105
tom1700 <tomekm1700@gmail.com>
2017-11-19 15:25:38 +0100

    Adding redux-form library

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import accountsReducer from './account/reducer';
 import transactionsReducer from './transaction/reducer';
 import categoriesReducer from './transactionCategory/reducer';
@@ -7,5 +8,6 @@ import categoriesReducer from './transactionCategory/reducer';
 export default combineReducers({
     accounts: accountsReducer,
     transactions: transactionsReducer,
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    form: formReducer
 });
