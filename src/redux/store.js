85228c5ff9d0da4e50d4d6485b2f484b2d9df6c9
tom1700 <tomekm1700@gmail.com>
2018-01-14 10:30:27 +0100

    Fixing testing

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import combinedReducers from './reducers';
 import middlewares from './middlewares';
 
+const testing = process.env.NODE_ENV === 'test';
+const storeEnhancers = testing ? applyMiddleware(...middlewares) : compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 const store = createStore(
     combinedReducers,
-    compose(
-        applyMiddleware(...middlewares),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    storeEnhancers
 );
 
 export default store;
