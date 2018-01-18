c34fa4f29a57d49a388977b1b08a498996f62283
tom1700 <tomekm1700@gmail.com>
2018-01-18 17:34:31 +0100

    Changing css to scss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Routes from './Routes';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
     render() {
