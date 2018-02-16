24f106442990dd8ad556ef8c5eb7eed9ba5a7ae7
tom1700 <tomekm1700@gmail.com>
2018-02-16 11:22:42 +0100

    Fixing safari and screen height.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,13 @@ import combinedReducers from './reducers';
 import middlewares from './middlewares';
 
 const testing = process.env.NODE_ENV === 'test';
+const devTools = (window.navigator.userAgent.includes('Chrome') ?
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    :
+    compose);
 const storeEnhancers = testing ? applyMiddleware(...middlewares) : compose(
     applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
 );
 
 const store = createStore(
