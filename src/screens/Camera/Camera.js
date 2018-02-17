4ea27532d7167d14c8fc7118ca8fad1916be796b
tom1700 <tomekm1700@gmail.com>
2018-02-17 09:28:29 +0100

    Styling camera

--- src/screens/Camera/Camera.js
+++ src/screens/Camera/Camera.js
@@ -7,12 +7,16 @@ class Camera extends Component {
 
         this.state = {
             stream: null,
+            snapTaken: false,
             error: ''
         }
     }
+
     async componentDidMount () {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: true
+            });
             this.videoNode.srcObject = stream;
             this.videoNode.play();
             this.setState({
@@ -25,24 +29,52 @@ class Camera extends Component {
             });
         }
     }
+
     componentWillUnmount() {
         this.state.stream.getTracks().forEach((track) => {
             track.stop();
         });
     }
 
-    renderVideo() {
-        return (<video
-            ref={videoNode => { this.videoNode = videoNode; }}
-            className="camera"
-        />);
+    snapshot() {
+        this.canvasNode.width = this.videoNode.videoWidth;
+        this.canvasNode.height = this.videoNode.videoHeight;
+        const ctx = this.canvasNode.getContext('2d');
+        ctx.imageSmoothingEnabled = true;
+        ctx.drawImage(this.videoNode, 0, 0, this.canvasNode.width, this.canvasNode.height);
+        this.imgNode.src = this.canvasNode.toDataURL('image/webp');
+        this.setState({ snapTaken: true });
     }
-    render() {
+
+    renderError() {
+        return <div className="camera">{this.state.error}</div>;
+    }
+
+    renderVideo() {
+        const videoDisplay = this.state.snapTaken ? 'none' : 'block';
+        const imgDisplay = this.state.snapTaken ? 'block' : 'none';
+
         return (
-            <div>
-                { this.state.error ? this.state.error: this.renderVideo() }
+            <div className="camera">
+                <video
+                    ref={videoNode => { this.videoNode = videoNode; }}
+                    className="camera__video"
+                    onClick={() => { this.snapshot(); }}
+                    style={{ display: videoDisplay }}
+                />
+                <img
+                    src=""
+                    alt=""
+                    className="camera__img"
+                    ref={ imgNode => { this.imgNode = imgNode; }}
+                    style={{ display: imgDisplay }}
+                />
+                <canvas className="camera__canvas" ref={ canvasNode => { this.canvasNode = canvasNode; }} />
             </div>
         );
+    }
+    render() {
+        return this.state.error ? this.renderError() : this.renderVideo();
     }
 }
 
